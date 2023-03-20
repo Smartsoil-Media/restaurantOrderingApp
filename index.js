15440cf2bd109b044f610c513dd203080bcfa79d
GunClayton <114902151+Smartsoil-Media@users.noreply.github.com>
2023-03-20 15:09:43 +0800

    styles changes

--- index.js
+++ index.js
@@ -2,82 +2,90 @@ import {menuArray} from "./data.js"
 let myOrder = []
     
 document.addEventListener('click', function(e) {
-    handleGetItem(e.target.dataset)
- 
+  handleGetItem(e.target.dataset);
+  handleRemoveItem(e.target);
 })
 
+function getTotalOrderAmount() {
+  let totalAmount = 0
+  myOrder.forEach(function(item){
+    totalAmount += item.price;
+  }); return totalAmount
+}
+
 function handleGetItem(data) {
-    let selectedItem = ''
-    menuArray.forEach(function (item) {
-      if (item.id === parseInt(data.add)) {
-        selectedItem = item;
-        myOrder.push(selectedItem)
-        render()
-      } 
-    })
+  let selectedItem = ''
+  menuArray.forEach(function (item) {
+    if (item.id === parseInt(data.add)) {
+      selectedItem = item;
+      myOrder.push(selectedItem)
+      render()
+    } 
+  })
+}
+
+function handleRemoveItem(target) {
+  if (target.classList.contains('remove-btn')) {
+    const orderItem = target.closest('.indi-order-item');
+    myOrder.pop(orderItem);
+    render();
   }
+}
 
 function getFeedHtml() {
-
-    let feedHtml = ''
-    menuArray.forEach(function(item) {
-        feedHtml += `
-            <div class="single-item-container data-id=${item.id} flex">
-                <div class="emoji">
-                    <p> ${item.emoji} </p>
-                </div>
-                <div class="item-desc flex-col">
-                    <h2 class="item-name">${item.name}</h2>
-                    <p class="item-ingred italic "> ${item.ingredients.join(", ")} </p>
-                    <p class="item-price"> $${item.price} </p>
-                </div>
-                <div>
-                    <button class="add-btn" data-add="${item.id}"> + </button>
-                </div>
-            </div>
-            <div class="page-break"></div>
-            `
-    }); return feedHtml
+  let feedHtml = ''
+  menuArray.forEach(function(item) {
+    feedHtml += `
+      <div class="single-item-container data-id=${item.id} flex">
+        <div class="emoji">
+          <p> ${item.emoji} </p>
+        </div>
+        <div class="item-desc flex-col">
+          <h2 class="item-name">${item.name}</h2>
+          <p class="item-ingred italic "> ${item.ingredients.join(", ")} </p>
+          <p class="item-price"> $${item.price} </p>
+        </div>
+        <div id="button-container">
+          <button class="add-btn" data-add="${item.id}"> + </button>
+        </div>
+      </div>
+      <div class="page-break"></div>
+    `
+  }); return feedHtml
 }
 
 function getOrderHtml() {
-    let orderHtml = ''
-    myOrder.forEach(function(item){
-        orderHtml += 
-            `
-            <h1> ${item.name} </h1>
-            <h1> ${item.price} </h1>
-            `
-    }); return orderHtml 
+  let totalOrderAmount = getTotalOrderAmount();
+  document.getElementById('total-amount').innerHTML = "$" + totalOrderAmount
+  let orderHtml = ''
+  myOrder.forEach(function(item){
+    orderHtml += 
+      `
+      <div class="indi-order-item"">
+        <div class="order-name-and-price  flex">
+          <h2 class="ordered-item-title"> ${item.name} </h2>
+          <button class="remove-btn italic"> remove </button>
+        </div>
+        <div class="ordered-price-container">
+          <h2 class="ordered-item-price"> $${item.price} </h2>
+        </div>
+      </div>
+      `
+  });
+  
+  let orderContainer = document.getElementById('order-container')
+  orderContainer.innerHTML = orderHtml
+
+
+  return orderHtml
 }
 
 function render() {
-    let feedHtml = getFeedHtml()
-    let menuContainer = document.getElementById('menu-contianer')
-    menuContainer.innerHTML = feedHtml
+  let feedHtml = getFeedHtml()
+  let menuContainer = document.getElementById('menu-contianer')
+  menuContainer.innerHTML = feedHtml
 
-    let orderhtml = getOrderHtml()
-    let orderContainer = document.getElementById('order-container')
-    orderContainer.innerHTML = orderhtml
-
+  let orderHtml = getOrderHtml()
 }
 
 render()
-
-
-
-
-
-
-
-
-
-
-// forEach item clicked add it the myOrder
-
-//Render myOrder the the orderHtml
-
-//remove my order
-
-//Pop up payment detail on 
-
